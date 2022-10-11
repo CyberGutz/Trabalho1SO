21060bd4ff17b1f62dfc5ebd2ae594a5df757064
Gustavo <gutemagae@gmail.com>
2022-10-11 20:00:48 -0300

    Argv tokens concertados, embelezamento e organização do código

--- src/main.c
+++ src/main.c
@@ -6,30 +6,50 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main(int argc, char *argv[256]){
+int main(int argc, char *argv[]){
     pid_t task;
     int status, retorno;
+    
     char comando[50];
+    char *stoken;
+    
     char path[PATH_MAX];
     getcwd(path, sizeof(path));
-    argc = 0;
-    char *token;
+
+    system("clear");
+    printf("\n*->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->*\n");
+    printf("\n**************************** RUSH !!! ************************\n");
+    printf("\n**************************************************************\n");
+    printf("\n*********************** SEJA BEM VINDO ***********************\n");
+    printf("\n*->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->*\n\n");
+
+    
+    
 
     while(1){
-        fflush(stdin);
+        argc = 0;
         printf("\n%s $ ",path);
         fgets(comando, 50, stdin);
         comando[strcspn(comando,"\n")] = '\0';
-        token = strtok(comando, " ");
 
-
-        /*
-        while(token != 0){
-            argv[argc++] = token;
+        if(strcmp(comando, "exit") == 0){
+            printf("\n****************************LOGOUT****************************\n");
+            printf("\n                         ->System log:\n\n");
+            system("uptime");
+            printf("\n");
+            break;
         }
-        argv[argc] = NULL;
 
-        */
+        stoken = strtok(comando, " ");
+
+        while(stoken != NULL){
+            argc++;
+            argv[argc] = stoken;
+            stoken = strtok (NULL, " \n");
+            printf("\nArgc\t\t%d\n\nArgv\t\t%s\n\nToken\t\t%s\n\n",argc, argv[argc], stoken);
+        }
+        argv[argc++] = stoken;
+        printf("\nArgc\t\t%d\n\nArgv\t\t%s\n\nToken\t\t%s\n\n",argc, argv[argc], stoken);
         
         task = fork();
 
@@ -37,26 +57,16 @@ int main(int argc, char *argv[256]){
             perror("Error:");
             exit(1);
         }
+
         else if(task == 0 && strcmp(comando, "exit") != 0){
-            printf("\n");
             execvp(comando, argv);
+
             printf("\nError: it was not possible to execute %s\n",comando);
             exit(1);
         }
+
         else{
             retorno = waitpid(task, &status, 1);
-        }
-
-
-        if(strcmp(comando, "clear") == 0){
-            system("clear");
-        }
-
-        if(strcmp(comando, "exit") == 0){
-            printf("\nlogout\n");
-            printf("System log: \n");
-            system("uptime");
-            break;
         }
             
     }
